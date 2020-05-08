9b134b9cf1fe55a212885c14c4519dbf1ae06d00
Maxim Kulyk <mxx888777@gmail.com>
2020-05-08 21:44:58 +0300

    Update pch.h

--- pch.h
+++ pch.h
@@ -32,7 +32,6 @@
 #include "llvm/Transforms/Scalar/GVN.h"
 
 #include "llvm/ADT/APFloat.h"
-#include "llvm/ADT/STLExtras.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Constants.h"
 #include "llvm/IR/DerivedTypes.h"
@@ -40,15 +39,10 @@
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/LLVMContext.h"
-#include "llvm/IR/LegacyPassManager.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Type.h"
 #include "llvm/IR/Verifier.h"
 #include "llvm/Support/TargetSelect.h"
-#include "llvm/Target/TargetMachine.h"
-#include "llvm/Transforms/InstCombine/InstCombine.h"
-#include "llvm/Transforms/Scalar.h"
-#include "llvm/Transforms/Scalar/GVN.h"
 #include "llvm/Transforms/Utils.h"
 
 #include <algorithm>
@@ -61,4 +55,4 @@
 #include <charconv>
 #include <list>
 
-#endif //PCH_H
+#endif // PCH_H

2c5625382f717c8c1400049e05162b4633e5ca95
ZeeWanderer <mxx888777@gmail.com>
2021-11-20 22:05:22 +0200

    [Doxygen] use svg

--- doc_fix.py
+++ doc_fix.py
@@ -9,7 +9,7 @@ for path,dirs,files in os.walk("./doc"):
         if len(f) > 5: # prevent index out of range error
             if f[-4:] == ".dot":
                 source = os.path.join(path,f.replace(".dot",""))
-                cmdTpl = 'dot -Grankdir="LR" -Tpng -o<source>.png -Tcmapx -o<source>.map <source>.dot'
+                cmdTpl = 'dot -Grankdir="LR" -Tsvg -o<source>.svg -Tcmapx -o<source>.map <source>.dot'
                 cmd = cmdTpl.replace("<source>",source)
                 print(f"Running dot for {source}")
                 os.system(cmd)

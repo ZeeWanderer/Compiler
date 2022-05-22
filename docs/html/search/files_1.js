ec876760ab551bd360be6ccb4a1031d32a1aced8
ZeeWanderer <mxx888777@gmail.com>
2022-05-22 17:58:40 +0300

    [Documentation] properly exclude tests dir

--- docs/html/search/files_1.js
+++ docs/html/search/files_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['cmakelists_2etxt_0',['CMakeLists.txt',['../_c_make_lists_8txt.html',1,'(Global Namespace)'],['../tests_2_c_make_lists_8txt.html',1,'(Global Namespace)']]],
+  ['cmakelists_2etxt_0',['CMakeLists.txt',['../_c_make_lists_8txt.html',1,'']]],
   ['codegen_2ecpp_1',['CodeGen.cpp',['../_code_gen_8cpp.html',1,'']]],
   ['codegen_2eh_2',['CodeGen.h',['../_code_gen_8h.html',1,'']]],
   ['context_2ecpp_3',['Context.cpp',['../_context_8cpp.html',1,'']]],
